8b9aa95f3304479e374a4de46a54a03985475afb
cicidoll <ayaseemt@qq.com>
2020-11-27 22:07:25 +0800

    update routes-apiService.js

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const path = require('path');// 系统路径模块
+// const fs = require('fs'); // 文件模块
+const bodyParser = require('body-parser'); // 对post请求的请求体进行解析模块
+const app = express();// express框架模块
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
+//bodyParser.urlencoded 用来解析request中body的 urlencoded字符，只支持utf-8的编码的字符，也支持自动的解析gzip和 zlib。
+//返回的对象是一个键值对，当extended为false的时候，键值对中的值就为'String'或'Array'形式，为true的时候，则可为任何数据类型。
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +21,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+//Client网址
+const url = '*';
+//跨域问题解决方面
+const cors = require('cors');
+app.use(cors({  
+    origin:[url],
+    // methods:['GET','POST'],
+    methods:['GET']
+}));
+//跨域问题解决方面
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+　next();　
+});
+
+
+const apiService = require('./routes/apiService');
+app.use('/v1/test', apiService);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/apiService.js
@@ -0,0 +1,33 @@
+const express = require('express');           //使用express
+const router = express.Router();               //放数据
+
+const path = require('path');// 系统路径模块
+const fs = require('fs'); // 文件模块
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+	// const data = {
+	// 	code: 0,
+	// 	data: {
+	// 		name: 'aaa',
+	// 		pwd: '123'
+	// 	},
+	// 	isSuccess: true,
+	// 	msg: "请求成功"
+  // }
+  
+  // const file = path.join(__dirname, 'data/classRoomData.json'); //文件路径，__dirname为当前运行js文件的目录
+  const file = 'G:\\2020_StudyRecord\\ppsucTeam\\Express\\data\\classRoomData.json';
+  
+  //var file = 'f:\\nodejs\\data\\test.json'; //也可以用这种方式指定路径
+
+//读取json文件
+  fs.readFile(file, 'utf-8', function(err, data) {
+      if (err) {
+          res.send(err);
+      } else {
+          res.send(data);
+      }
+  });
+});
+module.exports = router;
